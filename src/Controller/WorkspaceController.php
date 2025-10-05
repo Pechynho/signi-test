7caedefb116b2315241043262523c90f76927040
Jan Pech <pechynho@gmail.com>
2025-10-05 11:49:19 +0200

    serializer

--- src/Controller/WorkspaceController.php
+++ src/Controller/WorkspaceController.php
@@ -2,19 +2,13 @@
 
 namespace App\Controller;
 
-use App\Repository\WorkspaceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
-
-use function dump;
 
 #[Route('/workspace')]
 final class WorkspaceController extends AbstractController
 {
     #[Route('', name: 'app_workspace_index', methods: ['GET'])]
-    public function index(): JsonResponse
-    {
-    }
+    public function index(): JsonResponse {}
 }
